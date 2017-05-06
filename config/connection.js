71a3bfd54dff5f9568459ba470c134401e7f894e
Thomas Carpenter <thomascarpenter@Thomass-MacBook-Pro.local>
2017-05-06 10:02:41 -0700

    heroku update

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,8 @@ if (process.env.JAWSDB_URL) {
 	password: "",
 	database: "burgers_db"
   });
-}
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
